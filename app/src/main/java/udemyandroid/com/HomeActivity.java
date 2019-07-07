91a9eb67ed4b035d86d2a0acc7a5406f0c62930b
Raymundo <rrosalesr1500@alumno.ipn.mx>
2019-07-06 21:34:37 -0500

    Se agrega ejemplo de ListView y eventos de click sobre elementos de la lista

--- app/src/main/java/udemyandroid/com/HomeActivity.java
+++ app/src/main/java/udemyandroid/com/HomeActivity.java
@@ -3,11 +3,24 @@ package udemyandroid.com;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
-    TextView email;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    //TextView email;
+
+    ListView lista;
+
+    List<String> androidVersionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +31,59 @@ public class HomeActivity extends AppCompatActivity {
         //aqui se almacenan todos los extras definidos en intent
         Bundle extras = getIntent().getExtras();
         String emailUsuario = extras.getString("valorEmail");
+        setTitle(emailUsuario);
 
+        //se busca componente de listView
+
+        lista = findViewById(R.id.listaVista);
+
+
+        //se carga lista de elementos
+
+        androidVersionList = new ArrayList<>();
+        androidVersionList.add("Pie");
+        androidVersionList.add("Oreo");
+        androidVersionList.add("Nougat");
+        androidVersionList.add("MarshMallow");
+        androidVersionList.add("Lollipop");
+        androidVersionList.add("Kitkat");
+        androidVersionList.add("...");
+
+        //adaptador toma todos los elementos de la lista y las va pintando
+        //dentro del listview
+
+        ArrayAdapter adaptadorVersionesAndroid = new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                androidVersionList
+        );
+
+        //vinculacion de listview con lista del adaptador
+
+
+        lista.setAdapter(adaptadorVersionesAndroid);
+
+
+        //gestion de evento click sobre elementos de la lista
+        lista.setOnItemClickListener(this);
+
+
+
+
+
+        /*
         email = findViewById(R.id.textViewEmail);
 
 
         email.setText("Bienvenido: "+emailUsuario);
+        */
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String androidVersion = androidVersionList.get(position);
+        Log.i("APP","version click "+androidVersion);
     }
 }
